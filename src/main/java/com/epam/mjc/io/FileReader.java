848fed4d213edd04f8c7fc314ccf20955c2222e0
MKN2003 <kazimovmiraziz197@gmail.com>
2023-11-24 22:19:51 +0500

    The changes have been made

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,24 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
 
 
 public class FileReader {
-
     public Profile getDataFromFile(File file) {
+        try {
+            BufferedReader reader = new BufferedReader(new java.io.FileReader(file));
+            String line;
+
+            while((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            reader.close();
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+        }
+
         return new Profile();
+    }
+    public static void main(String[] args) {
     }
 }
